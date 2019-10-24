b76f19833bca083d922cf2b0dd87523a4181b421
cartvdennis@gmail.com <NordstemmeN2000>
2019-10-24 20:25:51 +0200

    +dokumentation

--- desktop/src/de/klausmp/packman/desktop/DesktopLauncher.java
+++ desktop/src/de/klausmp/packman/desktop/DesktopLauncher.java
@@ -7,9 +7,14 @@ import de.klausmp.packman.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+//		einschaltung von vertikaler synkronisation
 		config.useVsync(true);
+//		setzen des fenster titels
 		config.setTitle("Pac Man");
+//		verhinderung der größenänderung des fensters
 		config.setResizable(false);
+//		setzen der größe des fensters
+		config.setWindowedMode(500, 500);
 		new Lwjgl3Application(new Main(), config);
 	}
 }
