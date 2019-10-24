89ee73416bc2b9335ca1dbad35d3800bf4eb5686
cartvdennis@gmail.com <NordstemmeN2000>
2019-10-24 20:07:38 +0200

    +gameScreen

---
+++ core/src/de/klausmp/packman/screens/GameScreen.java
@@ -0,0 +1,24 @@
+package de.klausmp.packman.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+
+public class GameScreen extends ScreenAdapter {
+    public GameScreen() {
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    }
+
+
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
