83ddb67962a6a0ecb9692609851c84a93b977ded
cartvdennis@gmail.com <NordstemmeN2000>
2019-10-24 20:22:39 +0200

    +desktoplauncher configuration

--- desktop/src/de/klausmp/packman/desktop/DesktopLauncher.java
+++ desktop/src/de/klausmp/packman/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import de.klausmp.packman.Main;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.useVsync(true);
+		config.setTitle("Pac Man");
+		config.setResizable(false);
 		new Lwjgl3Application(new Main(), config);
 	}
 }
