15652803b373eb6074a0f438ff589c8eba95fe81
cartvdennis@gmail.com <NordstemmeN2000>
2019-10-24 20:26:51 +0200

    -construcktor +verschiebung aus dem konstruktor in create() +dokumentation

--- core/src/de/klausmp/packman/Main.java
+++ core/src/de/klausmp/packman/Main.java
@@ -1,25 +1,27 @@
 package de.klausmp.packman;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import de.klausmp.packman.screens.GameScreen;
 
 public class Main extends Game {
 
 	private static Main INSTANCE;
-
-	public Main() {
+	@Override
+	public void create () {
 //		erstellung einer neuen instance von main wenn keine vorhanden ist
 		if (INSTANCE == null){
 			INSTANCE = this;
 		}
-	}
-
-	@Override
-	public void create () {
+//		setzen des Start Screens
+		setScreen(new GameScreen());
 	}
 
 	@Override
 	public void render () {
+//	    rendern des aktiven screens
+		getScreen().render(Gdx.graphics.getDeltaTime());
 	}
 
 	@Override
