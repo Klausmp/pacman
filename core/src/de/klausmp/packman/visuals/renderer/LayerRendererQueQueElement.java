e8d51fbeb32f611857252ccb91fe890b7afd185e
cartvdennis@gmail.com <NordstemmeN2000>
2019-10-27 22:00:52 +0100

    +neuer construktor mit default werten

--- core/src/de/klausmp/packman/visuals/renderer/LayerRendererQueQueElement.java
+++ core/src/de/klausmp/packman/visuals/renderer/LayerRendererQueQueElement.java
@@ -1,9 +1,12 @@
 package de.klausmp.packman.visuals.renderer;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 
 public class LayerRendererQueQueElement {
 
+    private static final float DEFAULTPROIRITY = 10.0f;
     public Sprite sprite;
     public Layers layerToRenderOn;
     public float priority;
@@ -23,6 +26,44 @@ public class LayerRendererQueQueElement {
     public LayerRendererQueQueElement(Sprite sprite) {
         this.sprite = sprite;
         this.layerToRenderOn = Layers.DEFAULT;
-        this.priority = 10.0f;
+        this.priority = DEFAULTPROIRITY;
+    }
+
+    public LayerRendererQueQueElement(Sprite sprite, Layers layerToRenderOn) {
+        this.sprite = sprite;
+        this.layerToRenderOn = layerToRenderOn;
+        this.priority = DEFAULTPROIRITY;
+    }
+
+    public LayerRendererQueQueElement(Texture texture, Vector2 position, Layers layerToRenderOn, float priority) {
+        sprite = new Sprite(texture);
+        sprite.setX(position.x);
+        sprite.setY(position.y);
+        this.layerToRenderOn = layerToRenderOn;
+        this.priority = priority;
+    }
+
+    public LayerRendererQueQueElement(Texture texture, Vector2 position, float priority) {
+        sprite = new Sprite(texture);
+        sprite.setX(position.x);
+        sprite.setY(position.y);
+        this.layerToRenderOn = Layers.DEFAULT;
+        this.priority = priority;
+    }
+
+    public LayerRendererQueQueElement(Texture texture, Vector2 position) {
+        sprite = new Sprite(texture);
+        sprite.setX(position.x);
+        sprite.setY(position.y);
+        this.layerToRenderOn = Layers.DEFAULT;
+        this.priority = DEFAULTPROIRITY;
+    }
+
+    public LayerRendererQueQueElement(Texture texture, Vector2 position, Layers layerToRenderOn) {
+        sprite = new Sprite(texture);
+        sprite.setX(position.x);
+        sprite.setY(position.y);
+        this.layerToRenderOn = layerToRenderOn;
+        this.priority = DEFAULTPROIRITY;
     }
 }
