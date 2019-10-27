cf050455f06103bde354fc8bb82b682dd13431a2
cartvdennis@gmail.com <NordstemmeN2000>
2019-10-27 20:40:15 +0100

    +rederung mit priorisierung in jedem layer

--- core/src/de/klausmp/packman/visuals/renderer/Layer.java
+++ core/src/de/klausmp/packman/visuals/renderer/Layer.java
@@ -3,12 +3,15 @@ package de.klausmp.packman.visuals.renderer;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Array;
 
-//TODO priority muss implementiert werden
-
 public class Layer {
 
     public Array<LayerRendererQueQueElement> elementsToRender;
     public Layers layerToRenderOn;
+
+    public Layer() {
+        this.layerToRenderOn = Layers.DEFAULT;
+        this.elementsToRender = new Array<LayerRendererQueQueElement>();
+    }
 
     public Layer(Layers layerToRenderOn) {
         this.layerToRenderOn = layerToRenderOn;
@@ -16,22 +19,42 @@ public class Layer {
     }
 
     public void render(SpriteBatch batch) {
+        sortElementsToRdender();
         for (LayerRendererQueQueElement element : elementsToRender) {
             element.sprite.draw(batch);
         }
         clear();
     }
 
-    public void sortElementsToRdender() {
-//        implementierung eines sortieralgerytmusses nach der priority
-//        dabei größte priority zu erst ind der liste
+    private void sortElementsToRdender() {
+        for (LayerRendererQueQueElement element : elementsToRender) {
+            System.out.println(element.priority);
+        }
+        for (int s = elementsToRender.size; s > -1; s--) {
+            for (int x = 0; x < s - 1; x++) {
+                if (elementsToRender.get(x).priority < elementsToRender.get(x + 1).priority) {
+                    swap(x, x + 1);
+                }
+            }
+        }
+        for (LayerRendererQueQueElement element : elementsToRender) {
+            System.out.println(element.priority);
+        }
+    }
+
+    private void swap(int firstIndex, int secondIndex) {
+        LayerRendererQueQueElement save = elementsToRender.get(firstIndex);
+        elementsToRender.removeIndex(firstIndex);
+        elementsToRender.insert(firstIndex, elementsToRender.get(secondIndex - 1));
+        elementsToRender.removeIndex(secondIndex);
+        elementsToRender.insert(secondIndex, save);
     }
 
     public void add(LayerRendererQueQueElement queQueElement) {
         elementsToRender.add(queQueElement);
     }
 
-    public void clear() {
+    private void clear() {
         elementsToRender.clear();
     }
 }
