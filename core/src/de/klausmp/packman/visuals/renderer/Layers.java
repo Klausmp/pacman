11a70bac59e210914701360d64a5da3f26f97bc2
cartvdennis@gmail.com <NordstemmeN2000>
2019-10-27 22:13:06 +0100

    +dokumentation

--- core/src/de/klausmp/packman/visuals/renderer/Layers.java
+++ core/src/de/klausmp/packman/visuals/renderer/Layers.java
@@ -2,15 +2,19 @@ package de.klausmp.packman.visuals.renderer;
 
 public enum Layers {
 
-    BACKGROUND(),
+
+    //  layer für den hintergrund
+    BACKGROUND,
+    //  layer für alles was im hintergrund des spieles abläuft
     BACK,
+    //  der normale layer in dem unter anderem der spieler gerendert wird, er representiert die mitte des spieles
     DEFAULT,
+    //  layer für alles was im fordergrund angezeigt werden soll
     FRONT,
+    //  layer für elemente die eigentlich nicht zum spiel gehören jedoch auch nicht zum text oder gui gehören
     INFRONT,
+    //  layer für texte die im spiel erscheinen
     TEXT,
+    //  layer für alle gui elemente
     GUI;
-
-    Layers() {
-
-    }
 }
