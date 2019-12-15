daa2a2b9489935f4c1bc83021abe1a8ac2105a59
cartvdennis@gmail.com <NordstemmeN2000>
2019-12-15 23:19:26 +0100

    +javadoc

--- core/src/de/klausmp/packman/gameObjects/ObjectType.java
+++ core/src/de/klausmp/packman/gameObjects/ObjectType.java
@@ -1,13 +1,30 @@
 package de.klausmp.packman.gameObjects;
 
 /**
+ * liste aller {@link GameObject GameObject} typen.
+ *
  * @author Klausmp
+ * @version  0.0.1
  */
-//TODO JAVA DOC MACHEN
 public enum ObjectType {
-
+    /**
+     * @see Wall
+     * @since 0.0.1
+     */
     WALL,
+    /**
+     * @see PacMan
+     * @since 0.0.1
+     */
     PACMAN,
+    /**
+     * @see Ghost
+     * @since 0.0.1
+     */
     GHOST,
+    /**
+     * @see Dot
+     * @since 0.0.1
+     */
     DOT;
 }
--- core/src/de/klausmp/packman/visuals/renderer/Layers.java
+++ core/src/de/klausmp/packman/visuals/renderer/Layers.java
@@ -1,36 +1,61 @@
 package de.klausmp.packman.visuals.renderer;
 
 /**
+ * liste aller verfügbaren {@link Layer layer} im renderer.
+ *
  * @author Klausmp
+ * @version 0.0.1
  */
-
-/*
- * dieses enum ist eine liste aller verfügbaren layer im renderer
- * zudem bietet sie eine DEFAULT
- * */
 public enum Layers {
 
-    //  layer für den hintergrund
+    /**
+     * layer für den hintergrund
+     *
+     * @since 0.0.1
+     */
     BACKGROUND,
-    //  layer für alles was im hintergrund des spieles abläuft
+    /**
+     * layer für alles was im hintergrund des spieles abläuft
+     *
+     * @since 0.0.1
+     */
     BACK,
-    //  der normale layer in dem unter anderem der spieler gerendert wird, er representiert die mitte des spieles
+    /**
+     * der normale layer in dem unter anderem der spieler gerendert wird, er representiert die mitte des spieles
+     *
+     * @since 0.0.1
+     */
     DEFAULT,
-    //  layer für alles was im fordergrund angezeigt werden soll
+    /**
+     * layer für alles was im fordergrund angezeigt werden soll
+     */
     FRONT,
-    //  layer für elemente die eigentlich nicht zum spiel gehören jedoch auch nicht zum text oder gui gehören
+    /**
+     * layer für elemente die eigentlich nicht zum spiel gehören jedoch auch nicht zum text oder gui gehören
+     *
+     * @since 0.0.1
+     */
     INFRONT,
-    //  layer für texte die im spiel erscheinen
+    /**
+     * layer für texte die im spiel erscheinen
+     *
+     * @since 0.0.1
+     */
     TEXT,
-    //  layer für alle gui elemente
+    /**
+     * layer für alle gui elemente
+     *
+     * @since 0.0.1
+     */
     GUI;
 
-    //  default reihenfolger der layer in der sie gerendert werden
-//  hierbei ist der erste layer der der zu erst gezeichnet wird
-//  alles ander wird darüber gezeichnet
+    /**
+     * {@link Layer layer} mit kleinem index werden unter {@link Layer layern} mit großem index gezeichnet. <br>
+     * dadurch sind {@link Layer layer} mit hohem index im vordergrund
+     *
+     * @return default zeichenreihenfolge der layer.
+     */
     public static Layers[] DEFAULTLAYERORDER() {
-        //TODO vereinfachung in einzeiler mit allem in return
-        Layers[] DEFAULTLAYERORDER = {Layers.BACKGROUND, Layers.BACK, Layers.DEFAULT, Layers.FRONT, Layers.INFRONT, Layers.TEXT, Layers.GUI};
-        return DEFAULTLAYERORDER;
+        return new Layers[]{Layers.BACKGROUND, Layers.BACK, Layers.DEFAULT, Layers.FRONT, Layers.INFRONT, Layers.TEXT, Layers.GUI};
     }
 }
